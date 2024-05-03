e0403781ebbf5053d0cde8f11bb473e883353d46
Llaury Juan Manuel <llauryjm@gmail.com>
2024-05-02 22:44:16 -0300

    ahora si

--- BackEnd/src/server.js
+++ BackEnd/src/server.js
@@ -2,3 +2,13 @@ import express from 'express';
 
 const app = express();
 app.use(express.json());
+
+
+
+app.get('/hello', (req, res) => {
+    res.send('Hello');
+});
+
+app.listen(8000, () => {
+    console.log('Server started at http://localhost:8000');
+}   );
