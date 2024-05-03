f9326178d5f3d0cd492b755623ceca23713b9b84
LautyGutierrez <lautygutierrez5@gmail.com>
2024-05-02 23:59:15 -0300

    data base

--- BackEnd/src/server.js
+++ BackEnd/src/server.js
@@ -1,6 +1,8 @@
 import express from 'express';
-
 const app = express();
+const initDB = require('../config/db')
+
+
 app.use(express.json());
 
 
@@ -12,3 +14,8 @@ app.get('/hello', (req, res) => {
 app.listen(8000, () => {
     console.log('Server started at http://localhost:8000');
 }   );
+
+
+
+//inicializacion de la base de datos
+initDB()
