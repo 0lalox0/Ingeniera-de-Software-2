e13959bcecabb1805762880784b4252583715d17
= <=>
2024-05-06 20:02:22 -0300

    cambie el otro gitignore y arregle el init.sh

--- init.sh
+++ init.sh
@@ -1,24 +1,25 @@
 #!/bin/bash
  
-# Ir al directorio del frontend
-cd FrontEnd/src
+# Ir al directorio del backend
+cd BackEnd/
+# Instalar las dependencias del backend
+#echo "Instalando dependencias del backend..."
+#npm install
+# iniciar el backend
+#npm install
+#echo "Iniciando el servidor backend..."
+#node app.js
+start "" "C:\Program Files\Git\bin\bash.exe" -c "npm run dev"
 
+# Ir al directorio del frontend
+cd ..
+cd FrontEnd/
+npm run dev
 # Instalar las dependencias del frontend
-echo "Instalando dependencias del frontend..."
+#echo "Instalando dependencias del frontend..."
 #npm install
 
 # Iniciar la aplicación frontend (React)
-echo "Iniciando la aplicación frontend..."
-start "" "C:\Program Files\Git\bin\bash.exe" -c "npm run dev" # Abre una consola aparte para poder cerrar el frontend mas facil
-
-# Ir al directorio del backend
-#cd BackEnd/src
-# Instalar las dependencias del backend
-echo "Instalando dependencias del backend..."
-#npm install
-# iniciar el backend
-#npm install express
-echo "Iniciando el servidor backend..."
-#node app.js
-#npm start 
+#echo "Iniciando la aplicación frontend..."
+#npm run dev # Abre una consola aparte para poder cerrar el frontend mas facil
 
