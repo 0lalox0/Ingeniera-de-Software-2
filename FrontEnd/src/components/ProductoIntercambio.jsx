f0f2d4ed5e44b213d99f47cc25cd6abfc1879b86
= <=>
2024-05-21 16:21:48 -0300

    anda

--- FrontEnd/src/components/ProductoIntercambio.jsx
+++ FrontEnd/src/components/ProductoIntercambio.jsx
@@ -3,12 +3,12 @@ import { useParams } from 'react-router-dom';
 
 export const ProductoIntercambio = () => {
     const [producto, setProducto] = useState(null);
-    const id = useParams();
+    const id = useParams().id;
 
     useEffect(() => {
         fetch(`http://localhost:8000/api/prodintercambios/${id}`)
             .then(response => response.json())
-            .then(data => { setProducto(data) })
+            .then(data =>  setProducto(data))
             .catch(error => console.error('Error:', error));
     }, []);
 
