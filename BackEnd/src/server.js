ff344aa396ba5d53aca735782624f53d35c908dc
LautyGutierrez <lautygutierrez5@gmail.com>
2024-05-22 12:01:02 -0300

    arreglo del tamanio de las fotos en el server

--- BackEnd/src/server.js
+++ BackEnd/src/server.js
@@ -11,6 +11,8 @@ import prodIntercambiosRouter from './routes/prodIntercambioRoute.js'
 //Couldinary
 import {v2 as cloudinary} from 'cloudinary';
 
+
+
 cloudinary.config({ 
   cloud_name: 'dr05fh5qy', 
   api_key: '175555578283967', 
@@ -33,8 +35,14 @@ admin.initializeApp({
 
 const app = express();
 
-app.use(express.json());
+
+
+//app.use(express.json());
 app.use(cors());
+
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({limit: '50mb', extended: true}));
+
 //Middleware que verifica el token
 app.use(async (req, res, next) => {
   const { authtoken } = req.headers;
