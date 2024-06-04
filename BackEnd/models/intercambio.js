c3cfe02b24195c081b7ba460e0301f0a581a5482
= <=>
2024-06-04 18:53:18 -0300

    cambie el modelo

--- BackEnd/models/intercambio.js
+++ BackEnd/models/intercambio.js
@@ -4,12 +4,12 @@ import { Schema, model } from 'mongoose'
 const IntercambioSchema = new Schema({
     productoOfrecido: { type: ObjectId, ref: 'ProductosParaIntercambiar', required: true },
     productoDeseado: { type: ObjectId, ref: 'ProductosParaIntercambiar', required: true },
-    usuarioOfrecido: { type: ObjectId, ref: 'User', required: true },
-    usuarioDeseado: { type: ObjectId, ref: 'User', required: true },
-    nombreSucursal: { type: String, required: true},
-    dia: { type: String, require: true },
+    usuarioOfrecido: { type: ObjectId, ref: 'User'},
+    usuarioDeseado: { type: ObjectId, ref: 'User'},
+    nombreSucursal: { type: String},
+    dia: { type: String},
     estado: { type: String, enum: ['pendiente', 'aceptado', 'rechazado'], default: 'pendiente' },
-    fecha: { type: Date, require: true }
+    fecha: { type: Date}
 })
 
 export default model("PropuestaDeIntercambio", IntercambioSchema)
