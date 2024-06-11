5c65f98cc83f89293ec8d9e10cca34d716beb651
= <=>
2024-06-11 15:41:13 -0300

    ahora se muestran los dos productos y hay botones de aceptar rechazar para el deseado

--- FrontEnd/src/components/ListarPropuestas.jsx
+++ FrontEnd/src/components/ListarPropuestas.jsx
@@ -17,19 +17,26 @@ export const ListarPropuestas = () => {
         const fetchProductos = async () => {
             const products = await Promise.all(propuestas.map(async (propuesta) => {
                 const response = await fetch(`http://localhost:8000/api/prodintercambios/${propuesta.productoOfrecido}`);
-                const data = await response.json();
-                return data;
+               
+                let ofrecido = await response.json();
+                const res = await fetch(`http://localhost:8000/api/prodintercambios/${propuesta.productoDeseado}`);
+                let deseado = await res.json();
+                return {
+                    ofrecido: ofrecido,
+                    deseado: deseado
+                };
             }));
             let e = localStorage.getItem("email");
-            setProductos(products.filter(o => o.idUsuario === e));
+            setProductos(products.filter(o => o.ofrecido.idUsuario === e || o.deseado.idUsuario === e));
         }
         fetchProductos();
     }, [propuestas]);
+    
 
     useEffect(() => {
         const fetchSucursales = async () => {
             const branches = await Promise.all(productos.map(async (producto) => {
-                const response = await fetch(`http://localhost:8000/api/sucursales/${producto.sucursal}`);
+                const response = await fetch(`http://localhost:8000/api/sucursales/${producto.deseado.sucursal}`);
                 const data = await response.json();
                 return data;
             }));
@@ -41,7 +48,7 @@ export const ListarPropuestas = () => {
     useEffect(() => {
         const fetchUsuarios = async () => {
             const users = await Promise.all(productos.map(async (producto) => {
-                const response = await fetch(`http://localhost:8000/api/users/${producto.idUsuario}`);
+                const response = await fetch(`http://localhost:8000/api/users/${producto.ofrecido.idUsuario}`);
                 const data = await response.json();
                 return data;
             }));
@@ -49,7 +56,13 @@ export const ListarPropuestas = () => {
         }
         fetchUsuarios();
     }, [productos]);
-
+    const aceptarIntercambio = (propuesta) => {
+        console.log(propuesta);
+    }
+    const rechazarIntercambio = (propuesta) => {
+        console.log(propuesta);
+    }
+  
     return (
         <div className='clase-propuestas'>
             <div className='titulo-propuestas'>
@@ -58,26 +71,37 @@ export const ListarPropuestas = () => {
             <table className="table table-hover">
                 <thead>
                     <tr>
-                        <th style={{ color: "#242465" }} scope="col">Nombre</th>
-                        <th style={{ color: "#242465" }} scope="col">Descripción</th>
-                        <th style={{ color: "#242465" }} scope="col">Foto</th>
+                        <th style={{ color: "#242465" }} scope="col">NombreOfrecido</th>
+                        <th style={{ color: "#242465" }} scope="col">NombreDeseado</th>
+                        <th style={{ color: "#242465" }} scope="col">Fotos</th>
                         <th style={{ color: "#242465" }} scope="col">Categoria</th>
                         <th style={{ color: "#242465" }} scope="col">Sucursal</th>
                         <th style={{ color: "#242465" }} scope="col">Solicitante</th>
+
                     </tr>
                 </thead>
                 <tbody className="table-group-divider">
                     {productos.map((producto, index) => {
                         return (
                             <tr key={propuestas[index]._id}>
-                                <td>{producto.titulo}</td>
-                                <td>{producto.descripcion}</td>
+                                <td>{producto.ofrecido.titulo}</td>
+                                <td>{producto.deseado.titulo}</td>
                                 <td>
-                                    <img src={producto.urlFotos[0]} width='80px' height='60px' />
+                                    <img src={producto.ofrecido.urlFotos[0]} width='80px' height='60px' />
+                                    <img src={producto.deseado.urlFotos[0]} width='80px' height='60px' />
                                 </td>
-                                <td>{producto.categoria}</td>
+                                <td>{producto.ofrecido.categoria}</td>
                                 <td>{sucursales[index]?.nombre}</td>
                                 <td>{usuarios[index]?.name} {usuarios[index]?.lastname}</td>
+                                {producto.deseado.idUsuario !== localStorage.getItem("email") ? <>
+                                <p style={{ color: '#439ac8' }}> ¿Te interesa este intercambio? ¡Aceptalo!</p>
+                                <td>
+                                    <button onClick={() => aceptarIntercambio(propuestas[index])} id='botonFecha' className="btn btn-success"> Aceptar Intercambio</button>
+                                    <button onClick={() => rechazarIntercambio(propuestas[index])} id='botonFecha' className="btn btn-success"> Rechazar Intercambio</button>
+
+                                </td>
+                                </> : <> </>}
+                                
                             </tr>
                         );
                     })}
