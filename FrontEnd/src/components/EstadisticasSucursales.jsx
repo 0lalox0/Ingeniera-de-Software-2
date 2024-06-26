9f266ff4a7e70fd0220a77d7266959b7887904a6
Llaury <llauryjm@gmail.com>
2024-06-26 12:03:00 -0300

    Estadisticas por sucursal - cant de intercambios

--- FrontEnd/src/components/EstadisticasSucursales.jsx
+++ FrontEnd/src/components/EstadisticasSucursales.jsx
@@ -1,11 +1,18 @@
 import React, { useEffect, useState, useRef } from 'react';
+import { useNavigate } from 'react-router-dom';
+import useUser from '../hooks/useUser';
+import { Mantenimiento } from './Mantenimiento';
 import * as d3 from 'd3';
 
 export const EstadisticasSucursales = () => {
-    const [sucursales, setSucursales] = useState([]);
-    const [intercambios, setIntercambios] = useState({});
+    const { role } = useUser();
+    const navigate = useNavigate();
     const [loading, setLoading] = useState(true);
     const d3Container = useRef(null);
+    const [sucursales, setSucursales] = useState([]);
+    const [intercambios, setIntercambios] = useState({});
+
+    const redirectAdmin = () => navigate('/admin');
 
     useEffect(() => {
         const fetchSucursales = async () => {
@@ -24,9 +31,9 @@ export const EstadisticasSucursales = () => {
             let intercambiosTemp = {};
             try {
                 for (let sucursal of sucursales) {
-                    const response = await fetch(`http://localhost:8000/api/filtrarPropuestaIntercambios?nombreSucursal=${encodeURIComponent(sucursal.nombre)}`);
+                    const response = await fetch(`http://localhost:8000/api/filtrarPropuestaIntercambios?nombreSucursal=${encodeURIComponent(sucursal.nombre + " ")}`);
                     const data = await response.json();
-                    intercambiosTemp[sucursal.nombre] = data.length;
+                    intercambiosTemp[sucursal.nombre] = data.length; // cantidad de intercambios por sucursal
                 }
                 setIntercambios(intercambiosTemp);
             } catch (error) {
@@ -41,16 +48,16 @@ export const EstadisticasSucursales = () => {
     useEffect(() => {
         if (!loading && d3Container.current) {
             // Ajustes de margen, ancho y alto
-            const margin = { top: 20, right: 30, bottom: 110, left: 50 }, // Margen inferior donde estan los nombres de las sucursales
+            const margin = { top: 60, right: 30, bottom: 190, left: 50 }, // Margen inferior donde estan los nombres de las sucursales
                 width = 460 - margin.left - margin.right,
-                height = 400 - margin.top - margin.bottom;
+                height = 350 - margin.top - margin.bottom;
 
             // Añade el SVG al contenedor
             const svg = d3.select(d3Container.current)
                 .attr("width", width + margin.left + margin.right)
                 .attr("height", height + margin.top + margin.bottom)
                 .append("g")
-                .attr("transform", `translate(${margin.left},${margin.top})`);
+                .attr("transform", `translate(${margin.left},${margin.top+100})`);
 
             const data = Object.keys(intercambios).map(key => ({ sucursal: key, value: intercambios[key] }));
 
@@ -66,7 +73,7 @@ export const EstadisticasSucursales = () => {
                 .selectAll("text")
                 .attr("transform", "translate(-10,0)rotate(-45)")
                 .style("text-anchor", "end");
-                
+
             // Escala para el eje Y (valores)
             const y = d3.scaleLinear()
                 .domain([0, d3.max(data, d => d.value)])
@@ -90,8 +97,18 @@ export const EstadisticasSucursales = () => {
     }, [loading, intercambios]);
 
     return (
-        <div>
-            {loading ? <p>Cargando...</p> : <svg ref={d3Container}></svg>}
-        </div>
+        <>
+            {role === 'admin' ?
+                <div className='clase-propuestas'>
+                    <div className='titulos titulo-propuestas'>
+                        <h1>Estadísticas - Sucursales</h1>
+                        <p className='textoRedireccion' onClick={redirectAdmin}> Volver al perfil</p>
+                    </div>
+                    <div>
+                        {loading ? <p>Cargando...</p> : <svg ref={d3Container}></svg>}
+                    </div>
+                </div>
+                : <> <Mantenimiento> </Mantenimiento></>}
+        </>
     );
 };
