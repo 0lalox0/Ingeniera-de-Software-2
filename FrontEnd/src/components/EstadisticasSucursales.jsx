5c8d283f71e505a9b31dabc799989e3deb35c532
Llaury Juan Manuel <llauryjm@gmail.com>
2024-06-30 13:43:47 -0300

    Estadisticas ganancias por sucursal (Falta posicionar bien elementos)

--- FrontEnd/src/components/EstadisticasSucursales.jsx
+++ FrontEnd/src/components/EstadisticasSucursales.jsx
@@ -9,8 +9,10 @@ export const EstadisticasSucursales = () => {
     const navigate = useNavigate();
     const [loading, setLoading] = useState(true);
     const d3Container = useRef(null);
+    const d3GananciasContainer = useRef(null);
     const [sucursales, setSucursales] = useState([]);
     const [intercambios, setIntercambios] = useState({});
+    const [ganancias, setGanancias] = useState({});
 
     const redirectAdminEstadisticas = () => navigate('/admin/estadisticas');
 
@@ -21,6 +23,7 @@ export const EstadisticasSucursales = () => {
                 const data = await response.json();
                 setSucursales(data);
                 fetchIntercambios(data);
+                fetchGanancias(data);
             } catch (error) {
                 console.error("Error fetching sucursales:", error);
                 setLoading(false);
@@ -46,6 +49,27 @@ export const EstadisticasSucursales = () => {
                 console.error("Error fetching intercambios:", error);
             }
             setLoading(false);
+        };
+
+        const fetchGanancias = async (sucursales) => {
+            try {
+                const response = await fetch('http://localhost:8000/api/productoCompra');
+                const productos = await response.json();
+                let gananciasTemp = {};
+
+                for (let sucursal of sucursales) {
+                    gananciasTemp[sucursal.nombre] = 0;
+                }
+
+                productos.forEach(producto => {
+                    const { nombreSucursal, precio } = producto;
+                    gananciasTemp[nombreSucursal] += precio;
+                });
+
+                setGanancias(gananciasTemp);
+            } catch (error) {
+                console.error("Error fetching ganancias:", error);
+            }
         };
 
         fetchSucursales();
@@ -101,7 +125,7 @@ export const EstadisticasSucursales = () => {
                 .range([height, 0]);
 
             svg.append("g")
-                .call(d3.axisLeft(y));
+                .call(d3.axisLeft(y).ticks(7)); //Limita el eje y a 7 elementos
 
             // Barras para total de intercambios y realizados
             const sucursal = svg.selectAll(".sucursal")
@@ -150,17 +174,71 @@ export const EstadisticasSucursales = () => {
         }
     }, [loading, intercambios]);
 
+    useEffect(() => {
+        if (!loading && d3GananciasContainer.current) {
+            const margin = { top: 60, right: 30, bottom: 190, left: 50 },
+                width = 460 - margin.left - margin.right,
+                height = 350 - margin.top - margin.bottom;
+
+            const svg = d3.select(d3GananciasContainer.current)
+                .attr("width", width + margin.left + margin.right)
+                .attr("height", height + margin.top + margin.bottom)
+                .append("g")
+                .attr("transform", `translate(${margin.left},${margin.top})`);
+
+            const data = Object.keys(ganancias).map(key => ({
+                sucursal: key,
+                ganancia: ganancias[key]
+            }));
+
+            const x = d3.scaleBand()
+                .range([0, width])
+                .domain(data.map(d => d.sucursal))
+                .padding(0.2);
+
+            svg.append("g")
+                .attr("transform", `translate(0,${height})`)
+                .call(d3.axisBottom(x))
+                .selectAll("text")
+                .attr("transform", "translate(-10,0)rotate(-45)")
+                .style("text-anchor", "end");
+
+            const y = d3.scaleLinear()
+                .domain([0, d3.max(data, d => d.ganancia)])
+                .range([height, 0]);
+
+            svg.append("g")
+                .call(d3.axisLeft(y).ticks(7));//Limita el eje y a 7 elementos
+
+            svg.selectAll("bars")
+                .data(data)
+                .enter()
+                .append("rect")
+                .attr("x", d => x(d.sucursal))
+                .attr("y", d => y(d.ganancia))
+                .attr("width", x.bandwidth())
+                .attr("height", d => height - y(d.ganancia))
+                .attr("fill", "#d04a35");
+        }
+    }, [loading, ganancias]);
+
     return (
         <>
             {role === 'admin' ?
                 <div className='clase-propuestas'>
-                    <div className='titulos titulo-propuestas'>
+                    <div className='titulos titulo-propuestas' style={{ marginTop: '0px' }}>
                         <h1>Estadísticas - Sucursales</h1>
                         <h2>Intercambios realizados por sucursal</h2>
                         <p className='textoRedireccion' onClick={redirectAdminEstadisticas}> Volver a estadísticas</p>
                     </div>
-                    <div>
+                    <div style={{ marginTop: '150px' }}>
                         {loading ? <p>Cargando...</p> : <svg ref={d3Container}></svg>}
+                    </div>
+                    <div className='titulos titulo-propuestas' style={{ marginTop: '400px' }}>
+                        <h2>Ganancias por sucursal</h2>
+                    </div>
+                    <div style={{ marginTop: '0px' }}>
+                        {loading ? <p>Cargando...</p> : <svg ref={d3GananciasContainer}></svg>}
                     </div>
                 </div>
                 : <> <Mantenimiento> </Mantenimiento></>}
